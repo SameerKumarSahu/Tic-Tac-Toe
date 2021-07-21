fb7a709e5e46639e3093e5442e9a0844ed08799a
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-21 13:42:04 +0530

    Show Boarder

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,12 +2,28 @@ package com.Bridgelabz;
 
 public class TicTacToeGame {
 
+static char[] board = new char[10];
 	
+	public void createBoard() {
+			for (int position = 1; position < board.length; position++) {
+			board[position] = ' ';
+		}
+	}
+	
+	public void showBoard() {
+		
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println(" ------- ");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println(" ------- ");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+		System.out.println("");
+	}
 	public static void main(String[] args) {
-		
-		System.out.println("------------- Welcome to TicTocToe Game ------------ "+"\n");
-		
-		
+		System.out.println("Tic Tak Toe Boarder"+"\n");
+		// creating object for create board
+		TicTacToeGame tictactoe = new TicTacToeGame();
+		tictactoe.showBoard();
 
 	}
 
