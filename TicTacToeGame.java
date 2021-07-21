b1ad96108c27fb7ee7e82d0caccc24119b4ac2b7
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-21 13:47:53 +0530

    Choose input

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.Bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 static char[] board = new char[10];
@@ -19,11 +21,26 @@ static char[] board = new char[10];
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 		System.out.println("");
 	}
+	
+	public static char chooseInput(Scanner scan) {
+		System.out.println(" Choose a letter either 'X' or 'O' : ");
+		return scan.next().toUpperCase().charAt(0);
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Tic Tak Toe Boarder"+"\n");
+		
 		// creating object for create board
 		TicTacToeGame tictactoe = new TicTacToeGame();
 		tictactoe.showBoard();
+		
+		//Allow the player to choose 
+		System.out.println("Enter the symbol X or O:");
+		Scanner sc = new Scanner(System.in);
+		char playerLetter = chooseInput(sc);
+		char computerSymbol = (playerLetter == 'X') ? 'O' : 'X';
+		System.out.println("Player Letter is : " + playerLetter);
+		System.out.println("Computer Letter is : " + computerSymbol);
 
 	}
 
