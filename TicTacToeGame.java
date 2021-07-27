2d3b54ed18b2bd2db2fd58c6eded539cefc4a042
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-27 07:54:45 +0530

    Player choose desire location

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,9 +3,10 @@ package com.Bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+	public static Scanner sc = new Scanner(System.in);
+	public static char playerChoice;
+	static char board [] = new char[10];
 
-static char[] board = new char[10];
-	
 	public void createBoard() {
 			for (int position = 1; position < board.length; position++) {
 			board[position] = ' ';
@@ -22,30 +23,80 @@ static char[] board = new char[10];
 		System.out.println("");
 	}
 	
-	public static char chooseInput(Scanner scan) {
-		System.out.println(" Choose a letter either 'X' or 'O' : ");
-		return scan.next().toUpperCase().charAt(0);
-
+	public char chooseInput() {
+		System.out.println("Want you want X or O");
+		
+       char choice=sc.next().charAt(0);
+      
+       char  computerChoice ;
+        if(choice=='X' || choice=='x')
+        {
+        	playerChoice=choice;
+        	computerChoice='O';
+            System.out.println("Your choose  "+playerChoice);
+            System.out.println("Computer choose "+computerChoice);
+        }
+        else if(choice=='O' || choice=='o')
+        {
+        	playerChoice=choice;
+        	computerChoice= 'X';
+            System.out.println("Your choose "+playerChoice);
+            System.out.println("Computer choose "+computerChoice);
+        }
+        else
+            System.out.println("invalid char");
+        return 0;
+    }
+	
+//	public void playGame() {
+//		
+//		int boardLocation;
+//		
+//			System.out.println("Player Enter the location (1-9) where to put symbol: ");
+//			boardLocation = sc.nextInt();
+//			
+//			
+//			board[boardLocation] = playerChoice;
+//			showBoard();
+//		 
+//	}
+//	
+	public void playerMakeMove( char playerLetter) {
+		boolean isPositionFree ;
+		int boardLocation;
+		do {
+			System.out.println("Player Enter the location (1-9) where to put symbol: ");
+			boardLocation = sc.nextInt();
+			isPositionFree = false;
+			if (boardLocation >= 1 && boardLocation <= 9) {
+				isPositionFree = checkIfPositionFree(boardLocation);
+			} else {
+				System.out.println("Invalid position entered.");
+			}
+		} 
+		while (!isPositionFree);
+		board[boardLocation] = playerLetter;
+		showBoard();
+		
+	}
+	public boolean checkIfPositionFree(int boardLocation) {
+		if (board[boardLocation] == ' ') {
+			return true;
+		} else {
+			System.out.println("Already filled");
+			return false;
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Tic Tak Toe Boarder"+"\n");
 		
 		// creating object for create board
 		TicTacToeGame tictactoe = new TicTacToeGame();
-		tictactoe.showBoard();
-		
-		//Allow the player to choose 
-		System.out.println("Enter the symbol X or O:");
-		Scanner sc = new Scanner(System.in);
-		char playerLetter = chooseInput(sc);
-		char computerSymbol = (playerLetter == 'X') ? 'O' : 'X';
-		System.out.println("Player Letter is : " + playerLetter);
-		System.out.println("Computer Letter is : " + computerSymbol);
-		
-		System.out.println("\n"+"Showing the Tic Tak Toe Boarder to the player  "+"\n");
-		
-		// creating object for showing the board to the player
 			tictactoe.showBoard();
+			tictactoe.chooseInput();
+			tictactoe.showBoard();
+//			tictactoe.playGame();
+			tictactoe.playerMakeMove(playerChoice);
 
 	}
 
